7d107e51b36505af2d871bcf27c3d8c62ff8e3e5
marcogermani87 <marcogermani87@users.noreply.github.com>
2024-04-21 12:08:46 +0000

    Fix styling

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,8 +3,8 @@
 namespace MarcoGermani87\FilamentHcaptcha\Tests;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
-use Orchestra\Testbench\TestCase as Orchestra;
 use MarcoGermani87\FilamentHcaptcha\FilamentHcaptchaServiceProvider;
+use Orchestra\Testbench\TestCase as Orchestra;
 
 class TestCase extends Orchestra
 {
